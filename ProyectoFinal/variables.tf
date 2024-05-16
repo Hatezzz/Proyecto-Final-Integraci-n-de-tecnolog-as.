97677eab82e6b93a4df297dc4f17311ff74eba9c
Hatezzz <148016552+Hatezzz@users.noreply.github.com>
2024-05-15 19:55:49 -0600

    version final

---
+++ ProyectoFinal/variables.tf
@@ -0,0 +1,15 @@
+variable "database_name" {}
+variable "database_password" {}
+variable "database_user" {}
+
+variable "region" {}
+variable "shared_credentials_file" {}
+variable "ami" {}
+variable "instance_type" {}
+variable "instance_class" {}
+variable "PUBLIC_KEY_PATH" {}
+variable "PRIV_KEY_PATH" {}
+variable "AZ1" {}
+variable "AZ2" {}
+variable "AZ3" {}
+
